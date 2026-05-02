9fa51380785660341905680ef0deddbf83302a26
Niko Medina <medina.niko@hotmail.com>
2026-05-02 18:30:05 -0300

    Buscar por Id y palabra clave, Menu principal y de busqueda

--- to_do.c
+++ to_do.c
@@ -17,9 +17,13 @@ Nodo* crearListaVacia();
 Nodo * crearNodo(Tarea T);
 void insertarNodo(Nodo ** inicio, Nodo *nodo);
 void mostrarTareas(Nodo * Lista);
+void mostrarUnaTarea(Tarea, char*);
 void cargarTareas(Nodo ** pendientes);
 void marcarRealizadas(Nodo ** pendientes, Nodo ** realizadas);
 void moverTarea(Nodo **pendientes, Nodo **realizadas, int id);
+void menuBuscar(Nodo *pendientes, Nodo * realizadas);
+int buscarPorID(Nodo *tareas, int id, char * estado);
+int buscarPorPalabra(Nodo *lista, char *clave, char *estado);
 void liberarLista(Nodo * lista);
 
 int main(){
@@ -31,10 +35,35 @@ int main(){
     realizadas = crearListaVacia();
     marcarRealizadas(&pendientes, &realizadas);
 
-    printf("Tareas pendientes:\n");
-    mostrarTareas(pendientes);
-    printf("Tareas realizadas:\n");
-    mostrarTareas(realizadas);
+    int opcion;
+    do{
+        printf("\n--- MENU ---\n");
+        printf("1. Marcar tareas realizadas\n");
+        printf("2. Ver pendientes\n");
+        printf("3. Ver realizadas\n");
+        printf("4. Buscar tareas\n");
+        printf("0. Salir\n");
+        scanf("%d", &opcion);
+
+        switch(opcion){
+            case 1:
+                marcarRealizadas(&pendientes, &realizadas);
+                break;
+            case 2:
+                printf("\nPendientes:\n");
+                mostrarTareas(pendientes);
+                break;
+            case 3:
+                printf("\nRealizadas:\n");
+                mostrarTareas(realizadas);
+                break;
+            case 4:
+                printf("\nBuscar por ID:\n");
+                menuBuscar(pendientes, realizadas);
+                break; 
+        }
+
+    }while(opcion != 0);
 
     liberarLista(pendientes);
     pendientes = NULL;
@@ -65,6 +94,14 @@ void mostrarTareas(Nodo * Lista){
         printf("Duracion: %d horas\n", Lista->T.Duracion);
         Lista = Lista->Siguiente;
     }
+}
+
+void mostrarUnaTarea(Tarea tarea, char * estado){
+    printf("Tarea numero: %d\n", tarea.TareaID);
+    printf("Descripcion: ");
+    puts(tarea.Descripcion);
+    printf("Duracion: %d horas\n", tarea.Duracion);
+    printf("Estado: %s\n", estado);
 }
 
 void cargarTareas(Nodo ** pendientes){
@@ -137,6 +174,80 @@ void moverTarea(Nodo **pendientes, Nodo **realizadas, int id){
     *realizadas = actual;
 
     printf("Tarea %d movida a realizadas\n", id);
+}
+
+void menuBuscar(Nodo *pendientes, Nodo * realizadas){
+    int opcion;
+    int encontradoPendientes;
+    int encontradoRealizadas;
+    do{
+        printf("\n--- CONSULTAS ---\n");
+        printf("1. Buscar por ID\n");
+        printf("2. Buscar por palabra clave\n");
+        printf("0. Volver\n");
+        scanf("%d", &opcion);
+        getchar();
+
+        if(opcion == 1){
+            int id;
+            printf("Ingrese ID: ");
+            scanf("%d", &id);
+
+            encontradoPendientes = buscarPorID(pendientes, id, "Pendiente");
+            encontradoRealizadas = buscarPorID(realizadas, id, "Realizada");
+            if(!encontradoPendientes & !encontradoRealizadas){
+                printf("No se encontro tarea con ID %d\n", id);    
+            }
+        }
+        else if(opcion == 2){
+            char clave[50];
+            printf("Ingrese palabra clave: ");
+            fgets(clave, 50, stdin);
+            clave[strcspn(clave, "\n")] = 0;
+
+            encontradoPendientes = buscarPorPalabra(pendientes, clave, "Pendiente");
+            encontradoRealizadas = buscarPorPalabra(realizadas, clave, "Realizada");
+            if(!encontradoPendientes & !encontradoRealizadas){
+                printf("No se encontraron tareas con la palabra clave '%s'\n", clave);    
+            }
+            
+        }
+
+    }while(opcion != 0);
+}
+
+// busca coincidencia exacta
+int buscarPorID(Nodo *tareas, int id, char * estado){
+    Nodo *aux;
+    int encontrado = 0;
+
+    aux = tareas;
+    while(aux != NULL & encontrado == 0){
+        if(aux->T.TareaID == id){
+            mostrarUnaTarea(aux->T, estado);
+            encontrado = 1;
+        }
+        aux = aux->Siguiente;
+    }
+
+    return encontrado;
+}
+
+// busca todas las coincidencias parciales
+int buscarPorPalabra(Nodo *lista, char *clave, char *estado){
+    Nodo *aux;
+    int encontrado = 0;
+
+    aux = lista;
+    while(aux != NULL){
+        if(strstr(aux->T.Descripcion, clave) != NULL){
+            mostrarUnaTarea(aux->T, estado);
+            encontrado = 1;
+        }
+        aux = aux->Siguiente;
+    }
+
+    return encontrado;
 }
 
 void liberarLista(Nodo *lista){
